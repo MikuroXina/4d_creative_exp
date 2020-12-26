3832f41161cfd44a7efee460f20d9f391059b2fd
MikuroXina <ryosukadnak@gmail.com>
2020-12-27 02:32:44 +0900

    refactor: Format and Add comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 #include <xc.h>
 
-#pragma config FPLLIDIV=DIV_2
-#pragma config FPLLMUL=MUL_20
-#pragma config FPLLODIV=DIV_2
-#pragma config FNOSC=FRCPLL
+#pragma config FNOSC=FRCPLL // Use Fast RC Oscillator (8 Mhz) with System PLL
+#pragma config FPLLIDIV=DIV_2 // ÷ 2
+#pragma config FPLLMUL=MUL_20 // × 20
+#pragma config FPLLODIV=DIV_2 // ÷ 2
+// System clock is 40 MHz
+
 #pragma config FSOSCEN=OFF
 #pragma config IESO=OFF
 #pragma config FPBDIV=DIV_1
 #pragma config FWDTEN=OFF
 #pragma config JTAGEN=OFF
 
-
-
 int main() {
-	ANSELA = ANSELB = 0;
-	TRISA = 0b1000; // RA3 is input
-	TRISB = 0b100; // RB2 is input
-	LATA = LATB = 0;
-        
+    ANSELA = ANSELB = 0;
+    TRISA = 0b1000; // RA3 is input
+    TRISB = 0b100; // RB2 is input
+    LATA = LATB = 0;
+
     while (1) {
-        
+
     }
 }
