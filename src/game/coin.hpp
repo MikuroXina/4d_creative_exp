27a1f921f5a919b9f7358454addead569e8a1cf9
MikuroXina <ryosukadnak@gmail.com>
2020-12-27 02:43:31 +0900

    refactor: Split coin class

---
+++ src/game/coin.hpp
@@ -0,0 +1,19 @@
+#ifndef COIN_HPP
+#define COIN_HPP
+
+class Coin {
+  static constexpr u8 MAX_COIN = 99;
+  u8 _amount = 0;
+
+public:
+  Coin(u8 amount) { _amount = std::min(std::max<u8>(amount, 0), MAX_COIN); }
+
+  Coin operator+(Coin const &r) const {
+    return Coin(std::max<u8>(_amount + r._amount, MAX_COIN));
+  }
+  Coin operator-(Coin const &r) const {
+    return Coin((u8)std::max<i8>((i8)_amount - (i8)r._amount, 0));
+  }
+};
+
+#endif // COIN_HPP
--- src/game/pone.hpp
+++ src/game/pone.hpp
@@ -2,25 +2,11 @@
 #define PONE_HPP
 
 #include "../general.hpp"
+#include "./coin.hpp"
 #include "./panel.hpp"
 
 #include <algorithm>
 #include <vector>
-
-class Coin {
-  static constexpr u8 MAX_COIN = 99;
-  u8 _amount = 0;
-
-public:
-  Coin(u8 amount) { _amount = std::min(std::max<u8>(amount, 0), MAX_COIN); }
-
-  Coin operator+(Coin const &r) const {
-    return Coin(std::max<u8>(_amount + r._amount, MAX_COIN));
-  }
-  Coin operator-(Coin const &r) const {
-    return Coin((u8)std::max<i8>((i8)_amount - (i8)r._amount, 0));
-  }
-};
 
 class Pone {
   Coin owned_coins;
