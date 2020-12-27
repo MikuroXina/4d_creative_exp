0000044434a4f1b76d91ff9b8da0482de7ab1deb
MikuroXina <ryosukadnak@gmail.com>
2020-12-28 03:38:51 +0900

    feat(lcd): Add classes

--- src/lcd_renderer/lcd.hpp
+++ src/lcd_renderer/lcd.hpp
@@ -1,4 +1,27 @@
 #ifndef LCD_HPP
 #define LCD_HPP
 
+#include "./texture.hpp"
+
+class OutputPort;
+
+class LcdGrid {
+  u8 is_left_row : 1;
+  u8 page : 3;
+
+public:
+  LcdGrid(u8 is_left_row, u8 column) : is_left_row(is_left_row), page(column) {}
+
+  void render(u8 bitmap, OutputPort &lcd);
+};
+
+class Lcd {
+  LcdGrid grids[16];
+
+public:
+  Lcd();
+
+  void render(Texture tex, OutputPort &lcd);
+};
+
 #endif // LCD_HPP
--- src/lcd_renderer/texture.hpp
+++ src/lcd_renderer/texture.hpp
@@ -1,35 +1,8 @@
 #ifndef TEXTURE_HPP
 #define TEXTURE_HPP
 
-#include "../game/board.hpp"
-#include "../game/camera.hpp"
-#include "../game/panel.hpp"
-#include "../game/pone.hpp"
 #include "../general.hpp"
 
-struct Texture64x64 {
-  u8 chunk[512];
-  Pos pos;
-
-  void render(Camera const &camera);
-};
-
-struct Path {
-  Pos from, to;
-
-  void render(Camera const &camera);
-};
-
-struct Roulette {
-  u8 num;
-
-  void renderRoulette(Camera const &camera);
-};
-
-void renderBoard(Camera const &camera, Board const &path);
-void renderPanel(Camera const &camera, Panel const &path);
-void renderPone(Camera const &camera, Pone const &path);
-
-void renderMessage(Camera const &camera, char const &message);
+using Texture = u8[1024];
 
 #endif // TEXTURE_HPP
