4e3c308a6ecf5ca974f69d30bc155f660b302f07
MikuroXina <ryosukadnak@gmail.com>
2021-01-24 23:28:29 +0900

    doc: Add doc

--- src/audio_output/wav_player.hpp
+++ src/audio_output/wav_player.hpp
@@ -8,6 +8,11 @@
 class InputPort;
 class OutputPort;
 
+// WavPlayer keeps playing and not playing Wav.
+// WavPlayer swaps between playing and not playing when output the whole of
+// playing Wav.
+// WavPlayer initiates not playing Wav with SoundEffect generations when it is
+// not dirty.
 class WavPlayer {
   std::vector<SoundEffect> sound_effects = {};
   Wav playing = {}, not_playing = {};
