7b13c7686b39b67a5031bfc694c023404de3faad
MikuroXina <ryosukadnak@gmail.com>
2021-01-26 00:58:15 +0900

    fix: Fix doc

--- src/game/roulette.hpp
+++ src/game/roulette.hpp
@@ -2,14 +2,16 @@
 #define ROULETTE_HPP
 
 #include "../general.hpp"
+#include "../ui/text.hpp"
 
 class Pone;
 
-// Roulette keeps Menu having MenuItem 1 to 4.
+// Roulette keeps Menu having Text says 1 to 4.
 // Roulette moves MenuFocus by time and decreases its speed.
 // Roulette results the integer from 1 to 4.
 template <class R> class Roulette {
   R _generator;
+  Text texts[4];
 
 public:
   Roulette(R generator) : _generator(generator) {}
