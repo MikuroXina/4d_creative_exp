e525d424c14c264dd29b5a3df398e8d7d5f3f790
MikuroXina <ryosukadnak@gmail.com>
2021-01-26 23:14:40 +0900

    feat: Impl send_lcd

--- src/lcd_renderer/frame.hpp
+++ src/lcd_renderer/frame.hpp
@@ -6,8 +6,9 @@
 class OutputPort;
 
 class Frame {
-  u8 texture[1024] = {};
-  u8 needs_flush[2][8] = {};
+  u8 texture[1024] = {}; // 128x64 = 1024x8
+  u8 needs_flush[2] =
+      {}; // needs_flush[page] = 7 6 5 4 3 2 1 0 whether needed flush
 
 public:
   friend void send_lcd(Frame const &tex, OutputPort &lcd);
--- src/lcd_renderer/lcd.hpp
+++ src/lcd_renderer/lcd.hpp
@@ -5,11 +5,43 @@
 
 class OutputPort;
 
-void send_lcd_grid(u8 bitmap, OutputPort &lcd, u8 row) {}
+inline void send_lcd_grid(u8 grid, OutputPort &lcd, u8 row) {}
 
-void send_lcd_line(u8 bitmaps[8][8], OutputPort &lcd, u8 column,
-                   bool is_left_row) {}
+inline void send_lcd_line(u8 line[64], OutputPort &lcd, u8 column,
+                          bool is_right_page) {}
 
-void send_lcd(Frame const &tex, OutputPort &lcd) {}
+inline void send_lcd(Frame const &tex, OutputPort &lcd) {
+  /*
+    Texture format:
+    0 | 00000000 11111111 22222222 ... 77777777
+    1 | 00000000 11111111 22222222 ... 77777777
+    :
+    :
+    7 | 00000000 11111111 22222222 ... 77777777
+
+    Line format:
+    0 1 2 ... 62 63
+    0 1 2 ... 62 63
+    : :
+    : :
+    0 1 2 ... 62 63
+  */
+  constexpr u8 TEX_WIDTH = 128, TEX_HEIGHT = 64;
+  for (u8 page{0}; page != 2; ++page) {
+    for (u8 line_column{0}; line_column != 8; ++line_column) {
+      if (!HAS_MASK(tex.needs_flush[page], 1 << line_column)) {
+        continue;
+      }
+      u8 line[64] = {};
+      for (u8 grid_column{0}; grid_column != 8; ++grid_column) {
+        for (u8 row{0}; row != TEX_HEIGHT; ++row) {
+          auto const column = line_column + grid_column;
+          line[row] |= tex.texture[column * TEX_WIDTH] & (1 << row);
+        }
+      }
+      send_lcd_line(line, lcd, line_column, page);
+    }
+  }
+}
 
 #endif // LCD_HPP
