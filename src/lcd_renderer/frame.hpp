cff30650c3542711b5092b4d2d16ee57c719ca26
MikuroXina <ryosukadnak@gmail.com>
2021-01-28 01:42:24 +0900

    feat: Impl draw16x16

--- src/lcd_renderer/frame.hpp
+++ src/lcd_renderer/frame.hpp
@@ -10,6 +10,8 @@ class Frame {
   u8 needs_flush[2] =
       {}; // needs_flush[page] = 7 6 5 4 3 2 1 0 whether needed flush
 
+  u8 tex_idx(Pos pos) const { return (pos.y() * 128 + pos.x()) / 8; }
+
   void mark_needs_flush(Rect area) {
     auto const top = area.pos().y() / 8,
                bottom = (area.pos().y() + area.size().h() + 7) / 8 % 8,
@@ -33,13 +35,22 @@ public:
     needs_flush[1] = 0;
   }
 
-  void draw16x16(u16 tex[16], Pos pos) {
-    auto const x = pos.x(), y = pos.y();
+  void draw16x16(u16 src[16], Pos pos) {
+    i8 const x = pos.x(), y = pos.y();
     mark_needs_flush(Rect{
         Pos{x, y},
         Size{16, 16},
     });
-    // TODO
+    for (u8 col{0}; col != 16; ++col) {
+      u16 const src_bits = src[col];
+      for (u8 row{0}; row != 16; ++row) {
+        i8 px_x = row + x, px_y = col + y;
+        if (0 <= px_x && px_x < 128 && 0 <= px_y && px_y < 64) {
+          u8 const idx = tex_idx(Pos{px_x, px_y});
+          texture[idx] ^= src_bits & 1 << (15 - row);
+        }
+      }
+    }
   }
 };
 
