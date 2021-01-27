fd57a3c96638c916fdd99b5fb6d8c9a97cc8c162
MikuroXina <ryosukadnak@gmail.com>
2021-01-28 01:21:52 +0900

    wip: Add draw16x16 and flush

--- src/lcd_renderer/frame.hpp
+++ src/lcd_renderer/frame.hpp
@@ -10,10 +10,37 @@ class Frame {
   u8 needs_flush[2] =
       {}; // needs_flush[page] = 7 6 5 4 3 2 1 0 whether needed flush
 
+  void mark_needs_flush(Rect area) {
+    auto const top = area.pos().y() / 8,
+               bottom = (area.pos().y() + area.size().h() + 7) / 8 % 8,
+               left = area.pos().x() / 64,
+               right = (area.pos().x() + area.size().w() + 63) / 64 % 2;
+    for (auto col{top}; col != bottom; ++col) {
+      for (auto row{left}; row != right; ++row) {
+        needs_flush[row] |= 1 << col;
+      }
+    }
+  }
+
 public:
   friend void send_lcd(Frame const &tex, OutputPort &lcd);
 
   Frame() {}
+
+  void flush(OutputPort &lcd) {
+    send_lcd(*this, lcd);
+    needs_flush[0] = 0;
+    needs_flush[1] = 0;
+  }
+
+  void draw16x16(u16 tex[16], Pos pos) {
+    auto const x = pos.x(), y = pos.y();
+    mark_needs_flush(Rect{
+        Pos{x, y},
+        Size{16, 16},
+    });
+    // TODO
+  }
 };
 
 #endif // FRAME_HPP
