c090b25af99ff25b0ef27e30114cd119a53daa69
MikuroXina <ryosukadnak@gmail.com>
2021-01-28 00:02:44 +0900

    feat: Impl lcd control

--- src/lcd_renderer/lcd.hpp
+++ src/lcd_renderer/lcd.hpp
@@ -1,14 +1,44 @@
 #ifndef LCD_HPP
 #define LCD_HPP
 
+#include "../general.hpp"
 #include "./frame.hpp"
+
+#include <xc.h>
 
 class OutputPort;
 
-inline void send_lcd_grid(u8 grid, OutputPort &lcd, u8 row) {}
+inline void send_to_lcd() {
+  LATBbits.RB6 = 1; // Send
+  busy_wait(100);
+  LATBbits.RB6 = 0;
+}
+
+inline void send_lcd_grid(u8 grid, OutputPort &lcd, u8 row) {
+  LATBCLR = ((u16)0xff) << 8;
+  LATBSET = grid << 8;
+  send_to_lcd();
+}
 
 inline void send_lcd_line(u8 line[64], OutputPort &lcd, u8 column,
-                          bool is_right_page) {}
+                          bool is_right_page) {
+  // Set the column
+  LATBCLR = reverse_bits((u16)0b111111);
+  LATBbits.RB4 = 0; // Instruction mode
+  const auto instruction = (0b10111000 | (column & 0b111)) << 8;
+  LATBSET = instruction;
+  send_to_lcd();
+
+  // Init the row to 0
+  LATBCLR = reverse_bits((u16)0b111111);
+  LATBSET = 0b01000000 << 8;
+  send_to_lcd();
+
+  LATBbits.RB4 = 1; // Data mode
+  for (u8 row{0}; row != 64; ++row) {
+    send_lcd_grid(line[row], lcd, row);
+  }
+}
 
 inline void send_lcd(Frame const &tex, OutputPort &lcd) {
   /*
@@ -28,6 +58,15 @@ inline void send_lcd(Frame const &tex, OutputPort &lcd) {
   */
   constexpr u8 TEX_WIDTH = 128, TEX_HEIGHT = 64;
   for (u8 page{0}; page != 2; ++page) {
+    if (page == 0) {
+      // Select left page
+      LATAbits.RA4 = 1;
+      LATBbits.RB3 = 0;
+    } else {
+      // Select right page
+      LATAbits.RA4 = 0;
+      LATBbits.RB3 = 1;
+    }
     for (u8 line_column{0}; line_column != 8; ++line_column) {
       if (!HAS_MASK(tex.needs_flush[page], 1 << line_column)) {
         continue;
