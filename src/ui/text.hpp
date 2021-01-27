2f047d3749873c210103da200a3c082add87fe0b
MikuroXina <ryosukadnak@gmail.com>
2021-01-28 01:46:58 +0900

    feat: Intro KatakanaString

--- src/ui/text.hpp
+++ src/ui/text.hpp
@@ -3,17 +3,20 @@
 
 #include "../lcd_renderer/frame.hpp"
 #include "./text/align.hpp"
+#include "./text/glyph.hpp"
 
 #include <string>
+
+using KatakanaString = std::basic_string<Katakana>;
 
 // Text is a Component and renders by Glyph data.
 // Text has TextAlign.
 class Text {
-  std::string _label;
+  KatakanaString _label;
   TextAlign _align;
 
 public:
-  Text(std::string label, TextAlign align) : _label(label), _align(align) {}
+  Text(KatakanaString label, TextAlign align) : _label(label), _align(align) {}
 
   void write(Frame &tex) const;
 };
