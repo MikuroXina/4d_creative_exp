c28dcf45a6b88d9ded194223cb97a8b975be8e26
MikuroXina <ryosukadnak@gmail.com>
2021-01-28 19:50:17 +0900

    feat: Add an example

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <xc.h>
 
+#include "controller/button.hpp"
+#include "general.hpp"
+#include "lcd_renderer/frame.hpp"
+#include "ui/text.hpp"
+
 // Use Fast RC Oscillator (8 Mhz) with System PLL
 #pragma config FNOSC = FRCPLL
 // ÷ 2
@@ -21,6 +26,21 @@ int main() {
   TRISA = 0b1000; // RA3 is input
   TRISB = 0b100;  // RB2 is input
   LATA = LATB = 0;
+
+  Button determine(0), left(1), right(2);
+
+  Frame frame{};
+
+  u16 line[16] = {};
+  for (u8 idx{0}; idx != 16; ++idx) {
+    line[idx] = 1 << idx;
+  }
+  frame.draw16x16(line, Pos{0, 0});
+
+  Text text(u"テスト");
+  text.write(frame);
+
+  frame.flush();
 
   while (1) {
   }
