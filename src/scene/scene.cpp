b212bd5b31cc9c944e1c4258911744b35a3f000a
MikuroXina <ryosukadnak@gmail.com>
2021-02-13 19:35:43 +0900

    feat: Impl applying an event

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -7,9 +7,10 @@ Scene::Scene(u8 numbers_of_player, Board &&board)
 void Scene::run() {
   while (true) {
     u8 steps = game.spin_roulette();
-    auto current = game.current_player();
+    auto &current = game.current_player();
     game.advance(current, steps);
-    // TODO: Activate an effect of the panel
+    auto &event = current.where_is_on().event();
+    event.apply(current);
     game.next_turn();
   }
 }
