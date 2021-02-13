e057fb2928fc44305bf795863a967f94c151ca45
MikuroXina <ryosukadnak@gmail.com>
2021-02-14 05:12:12 +0900

    refactor: Rename _pos into _anchor

--- src/ui/text.hpp
+++ src/ui/text.hpp
@@ -13,23 +13,23 @@ using KatakanaString = std::basic_string<Katakana>;
 // Text has TextAlign.
 class Text {
   KatakanaString _label;
-  Pos _pos;
+  Pos _anchor;
   TextAlign _align;
 
 public:
-  Text(KatakanaString label, Pos pos = Pos::zero(),
+  Text(KatakanaString label, Pos anchor = Pos::zero(),
        TextAlign align = TextAlign::Left)
-      : _label(label), _pos(pos), _align(align) {}
+      : _label(label), _anchor(anchor), _align(align) {}
 
   void write(Frame &tex) const {
-    auto x = _pos.x(), y = _pos.y();
+    auto x = _anchor.x(), y = _anchor.y();
     std::vector<u16> buf(16);
     for (auto const character : _label) {
       Glyph::katakana(character).write(buf.data());
       tex.draw16x16(buf.data(), Pos{x, y});
       x += 16;
       if (128 < x) {
-        x = _pos.x();
+        x = _anchor.x();
         y += 16;
       }
     }
