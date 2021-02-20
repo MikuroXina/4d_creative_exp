6973fade59ff38aab28f6f45295b2bbbd96610fa
MikuroXina <ryosukadnak@gmail.com>
2021-02-21 02:07:12 +0900

    chore: Add TODOs

--- src/audio_output/lib.hpp
+++ src/audio_output/lib.hpp
@@ -1,19 +1,37 @@
 #include "../general.hpp"
 
 // Rings when the player got its turn.
-inline u32 your_turn(u32 t) {}
+inline u32 your_turn(u32 t) {
+  // TODO
+  return 0;
+}
 
 // Rings when the piece advanced by one step.
-inline u32 step(u32 t) {}
+inline u32 step(u32 t) {
+  // TODO
+  return 0;
+}
 
 // Rings when the player's point gained.
-inline u32 point_gained(u32 t) {}
+inline u32 point_gained(u32 t) {
+  // TODO
+  return 0;
+}
 
 // Rings when the player's point lost.
-inline u32 point_lost(u32 t) {}
+inline u32 point_lost(u32 t) {
+  // TODO
+  return 0;
+}
 
 // Rings when the cursor is moved.
-inline u32 click(u32 t) {}
+inline u32 click(u32 t) {
+  // TODO
+  return 0;
+}
 
 // Rings when a menu item is entered.
-inline u32 entered(u32 t) {}
+inline u32 entered(u32 t) {
+  // TODO
+  return 0;
+}
