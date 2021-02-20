ff5dcce79615404a08e68b8b20fd56f7d9f05d9b
MikuroXina <ryosukadnak@gmail.com>
2021-02-21 02:06:48 +0900

    feat: Add sound lib

---
+++ src/audio_output/lib.hpp
@@ -0,0 +1,19 @@
+#include "../general.hpp"
+
+// Rings when the player got its turn.
+inline u32 your_turn(u32 t) {}
+
+// Rings when the piece advanced by one step.
+inline u32 step(u32 t) {}
+
+// Rings when the player's point gained.
+inline u32 point_gained(u32 t) {}
+
+// Rings when the player's point lost.
+inline u32 point_lost(u32 t) {}
+
+// Rings when the cursor is moved.
+inline u32 click(u32 t) {}
+
+// Rings when a menu item is entered.
+inline u32 entered(u32 t) {}
