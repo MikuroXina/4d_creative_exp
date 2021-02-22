695d3b8433620363bf908e278681e0f173d14b32
MikuroXina <ryosukadnak@gmail.com>
2021-02-22 19:30:09 +0900

    feat: Add Focus member

--- src/scene/scene.hpp
+++ src/scene/scene.hpp
@@ -1,21 +1,24 @@
 #ifndef SCENE_HPP
 #define SCENE_HPP
 
+#include "../controller/focus.hpp"
 #include "../game/game.hpp"
 #include "./camera.hpp"
 
 // Scene has Game and Camera.
 // Scene updates the game process.
 class Scene {
-  Game game;
+  Game _game;
   Camera camera;
+  Focus _focus;
 
 public:
   Scene(u8 numbers_of_player, Board &&board);
 
   void run();
 
-  Game &game() { return game; }
+  Game &game() { return _game; }
+  Focus &focus() { return _focus; }
 };
 
 #endif // SCENE_HPP
