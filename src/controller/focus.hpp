b6591d70fb73b4cf47cdae91a823805b6e85b42d
MikuroXina <ryosukadnak@gmail.com>
2021-02-22 15:26:31 +0900

    feat: Migrate Focus

--- src/controller/focus.hpp
+++ src/controller/focus.hpp
@@ -1,14 +1,15 @@
 #ifndef FOCUS_HPP
 #define FOCUS_HPP
 
+#include "../general.hpp"
+#include "../ui/menu.hpp"
 #include "button.hpp"
-
-class Menu;
 
 // Focus occupies button inputs and controls Menu.
 class Focus {
   HardwareButton left, right, confirm;
   Menu *target = nullptr;
+  u8 current_button_index;
 
 public:
   Focus(HardwareButton left, HardwareButton right, HardwareButton confirm)
@@ -17,6 +18,26 @@ public:
   void update(u8 elapsed);
 
   void set_target(Menu &menu) { target = &menu; }
+
+  void next_button() {
+    if (!target) {
+      return;
+    }
+    ++current_button_index;
+    if (current_button_index == target->buttons_count()) {
+      current_button_index = 0;
+    }
+  }
+  void prev_button() {
+    if (!target) {
+      return;
+    }
+    --current_button_index;
+    if (current_button_index == 255) {
+      current_button_index = target->buttons_count() - 1;
+    }
+  }
+  u8 current_button_idx() const { return current_button_index; }
 };
 
 #endif // FOCUS_HPP
